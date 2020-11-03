aaf6785a72f630cc87aa4da02b740b2a55caa26b
VIVEK RAJEEV <53962143+vivekcs18@users.noreply.github.com>
2020-11-03 15:20:09 +0530

    Add files via upload

---
+++ Lab 5 (03-11-2020)/nov3i.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Enter the character"
+
+read op
+vowel="It is a vowel"
+consonant="It is a consonant"
+
+case $op in
+a) echo $vowel
+   ;;
+e) echo $vowel
+   ;;
+i) echo $vowel
+   ;;
+o) echo $vowel
+   ;;
+u) echo $vowel
+   ;;
+*) echo $consonant
+   ;;
+esac
+
+
